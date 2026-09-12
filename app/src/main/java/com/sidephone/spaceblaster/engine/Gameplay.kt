b44358707dbd1b154e2fd42b279e7d408b979802
sspanak <doftor.livain@gmail.com>
2026-09-12 19:02:46 +0300

    final polishing

--- app/src/main/java/com/sidephone/spaceblaster/engine/Gameplay.kt
+++ app/src/main/java/com/sidephone/spaceblaster/engine/Gameplay.kt
@@ -392,19 +392,19 @@ class Gameplay(private val settings: Settings?) {
 			player.turn(now, left = leftPressed)
 		}
 
-		if (KeyEvent.KEYCODE_BUTTON_X in keys || KeyEvent.KEYCODE_DPAD_DOWN in keys) {
+		if (KeyEvent.KEYCODE_BUTTON_Y in keys || KeyEvent.KEYCODE_DPAD_DOWN in keys) {
 			player.stop(now)
 		} else {
-			player.thrust(now, KeyEvent.KEYCODE_BUTTON_A in keys || KeyEvent.KEYCODE_DPAD_UP in keys)
+			player.thrust(now, KeyEvent.KEYCODE_BUTTON_B in keys || KeyEvent.KEYCODE_DPAD_UP in keys)
 		}
 
-		if ((KeyEvent.KEYCODE_BUTTON_B in keys || KeyEvent.KEYCODE_SPACE in keys) && !player.isDead(now)) {
+		if ((KeyEvent.KEYCODE_BUTTON_A in keys || KeyEvent.KEYCODE_SPACE in keys) && !player.isDead(now)) {
 			playerBullets.shoot(now, player.cannonPosition(), player.direction())
 		} else {
 			playerBullets.resetShootTime(0L)
 		}
 
-		if (KeyEvent.KEYCODE_BUTTON_Y in keys || KeyEvent.KEYCODE_DEL in keys) {
+		if (KeyEvent.KEYCODE_BUTTON_X in keys || KeyEvent.KEYCODE_DEL in keys) {
 			jumpKeyDown = true
 		} else if (jumpKeyDown) {
 			jumpKeyDown = false
