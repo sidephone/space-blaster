0b8b173be926728432cc72e0de2aa65bdd271f5b
sspanak <doftor.livain@gmail.com>
2026-09-14 12:29:43 +0300

    bump up the version code for internal testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 		applicationId = "com.sidephone.spaceblaster"
 		minSdk = 31
 		targetSdk = 37
-		versionCode = 1
+		versionCode = 2
 		versionName = "1.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
